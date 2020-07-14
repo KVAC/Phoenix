1dbeff8c9e9f430f3971db3bcd87d7afba8843af
KVAC <kvacgeorgij@gmail.com>
2020-07-14 15:26:48 +0000

    clear 2

--- phoenix-libs/phoenix-libs-network/src/main/java/com/github/kvac/phoenix/libs/network/HEADER_NETWORK.java
+++ phoenix-libs/phoenix-libs-network/src/main/java/com/github/kvac/phoenix/libs/network/HEADER_NETWORK.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class HEADER_NETWORK {
-	@Getter
-	@Setter
-	private static int timeout = 10000;
 
+    @Getter
+    @Setter
+    private static int timeout = 10000;
 }
